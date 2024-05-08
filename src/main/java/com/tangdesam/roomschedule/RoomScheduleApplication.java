de338fea082103c8225fd6cbfec090cc9249d66f
tangdesam <tangdesam@gmail.com>
2024-05-08 16:31:20 +0700

    starter project

---
+++ src/main/java/com/tangdesam/roomschedule/RoomScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.tangdesam.roomschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tangdesam/roomschedule/RoomScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangdesam.roomschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
