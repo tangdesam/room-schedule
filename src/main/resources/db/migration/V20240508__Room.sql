4b1e3c09d2368b877f68f1da96e1f77044f1b8cf
tangdesam <tangdesam@gmail.com>
2024-05-08 17:03:14 +0700

    add: SQL script to create 'room' table and add sample data

---
+++ src/main/resources/db/migration/V20240508__Room.sql
@@ -0,0 +1,9 @@
+CREATE TABLE room (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(50),
+    capacity INT
+);
+
+INSERT INTO room VALUES('c65f1f33-61de-438a-a7df-d3b860cb5276', 'Jasmine', 30);
+INSERT INTO room VALUES('8a822329-bd07-4d05-a1f5-1820e2242cde', 'Lavender', 50);
+INSERT INTO room VALUES('c3f72d8d-6b00-4cdc-9ae0-a5bc56702f09', 'Lotus', 15);
